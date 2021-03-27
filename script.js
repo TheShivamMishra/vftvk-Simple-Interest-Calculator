9d1ca6792f58644558e1073893d5844f36f19f8e
Shivam <imshivam44@gmail.com>
2021-03-27 16:28:26 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    p = parseInt(p);
+    if(p<=0){
+        alert("Please enter a positive no");
+    }
     r = document.getElementById("rate").value;
     t = parseInt(document.getElementById("years").value);
     si = p*r*t/100;
     y = new Date().getFullYear();
     var year = parseInt(y) + t; 
-    document.getElementById("result").innerHTML = "If you deposite " + p + ",<br>at an interest rate of " + r +".<br> You will recieve an amount of " + si +",<br>in the year " + year;
+    if(p>0)
+    document.getElementById("result").innerHTML = "If you deposite <span>" + p + "</span>,<br>at an interest rate of <span>" + r +"</span>.<br> You will recieve an amount of <span>" + si +"</span>,<br>in the year <span>" + year + "<span>";
 }
 
 function update(val){
